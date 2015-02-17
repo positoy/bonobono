5f1a0f9d27b37dec5a16bfc71a37d975545263a5
positoy <positoy@localhost.localdomain>
2015-02-17 13:52:54 +0900

    최신

--- bono/user_data/projects/socctest/_js/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/js/Desktop/bonobono/bono/user_data/projects/socctest/_js/bin/AndroidManifest.xml : \
-/home/js/Desktop/bonobono/bono/user_data/projects/socctest/_js/AndroidManifest.xml \
--- bono/user_data/projects/socctest/_js/gen/org/tacademy/pager/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.tacademy.pager;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = false;
-}
