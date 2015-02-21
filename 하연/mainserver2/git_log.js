a26d4200804561f5a18be93fc8dff7d69fd77f8f
p22ju <p22ju@naver.com>
2015-02-21 22:01:27 +0900

    git_log module 2차 완성본, test_main2.js 1차 완성

--- 하연/mainserver2/git_log.js
+++ 하연/mainserver2/git_log.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec;
 var async= require('async');
 
 var __DIR = './user_data/projects/';
-global.finalArray = [];
+//global.finalArray = [];
 
 module.exports = function() {
 	return {
@@ -11,32 +11,40 @@ module.exports = function() {
 }
 
 
-function logTree_ (project_name, draw_handler) 
+function logTree_ (project_name, user_name, draw_handler) 
 {
 	var DIR_PROJECT = __DIR + project_name;
 	var DIR_PROJECT_ORIGIN = DIR_PROJECT + "/origin";
-	//var DIR_PROJECT_USER = DIR_PROJECT + "/_" + user_name;
+	var DIR_PROJECT_USER = DIR_PROJECT + "/_" + user_name;
 
-	var task1 = function(callback) {
+	var finalArray = [];
+	var finalObject = {};
+
+	var task1 = function(callback) 
+	{
 		var cmd1 = "cd " + DIR_PROJECT_ORIGIN;
 		var cmd2 = "git log --pretty=format:\"%h##%cn##%s##%cd##%cr\"";
-		
 		var cmd = cmd1 + ";" + cmd2;
 
-		var child = exec(cmd, function(error, stdout, stderr) {
+		var child = exec(cmd, function(error, stdout, stderr) 
+		{
 			if (error !== null)
 			{
-				console.log("logTree cmd error");
-				console.log(error);
+				//console.log("task1 cmd error");
+				//console.log(error);
+				finalObject.origin_no_commits_flag = 1;
+
+				callback(null, finalArray);
 			}
 			else
 			{
 				//console.log("logTree cmd success------");
 				//console.log(stdout);
 				//console.log("stdout's type: " + typeof(stdout));	//string
-
-				var logArray = stdout.split('\n');	//log split
-				for(idx in logArray) {
+				finalObject.origin_no_commits_flag = 0;
+				var logArray = stdout.split('\n');	//log split				
+				for(idx in logArray) 
+				{
 					var data = new Object();	// create object (==JSON object)
 					var splitArray = logArray[idx].split('##');	//info split
 					data.commit_hash = splitArray[0];
@@ -46,12 +54,43 @@ function logTree_ (project_name, draw_handler)
 					data.commit_date_rel = splitArray[4];
 					finalArray.push(data);
 				}
+				
+				//console.log("task1===========================================");
+				//console.log("log(finalArray)의 개수: " + finalArray.length);
+				//console.log("finalArray의 type: " + typeof(finalArray));	//object (array)
 
+				callback(null, finalArray);
+			}
+		});
+	};
 
-				console.log("log(finalArray)의 개수: " + finalArray.length);
-				console.log("finalArray의 type: " + typeof(finalArray));	//object (array)
-				console.log(finalArray);
+	var task2 = function(_finalArray, callback) 
+	{
+		var cmd1 = "cd " + DIR_PROJECT_USER;
+		var cmd2 = "git log --pretty=format:\"%h##%cn##%s##%cd##%cr\"";
+		var cmd = cmd1 + ";" + cmd2;
 
+		var child = exec(cmd, function(error, stdout, stderr) 
+		{
+			if (error !== null)
+			{
+				//console.log("task2 cmd error");
+				finalObject.user_no_commits_flag = 1;
+				finalObject.my_name = user_name;
+				callback(null, true);
+			}
+			else
+			{
+				finalObject.user_no_commits_flag = 0;
+				finalObject.my_name = user_name;
+				finalObject.origin_position = _finalArray[0].commit_hash;
+
+				var logArray2 = stdout.split('\n');	//log split
+				var splitArray2 = logArray2[0].split('##');	//info split
+				
+				finalObject.my_position = splitArray2[0];
+				finalObject.originLogTreeArray = _finalArray;
+		
 				callback(null, true);
 			}
 		});
@@ -61,11 +100,11 @@ function logTree_ (project_name, draw_handler)
 	{
 		if (result === true)
 		{
-			draw_handler(finalArray);	//draw_handler에 finalArray 전달
+			draw_handler(finalObject);	//draw_handler에 finalArray 전달
 			//handler();
 		}
 	};
 
-	async.waterfall([task1], callback);
+	async.waterfall([task1, task2], callback);
 
 };
--- 하연/mainserver2/test_main2.js
+++ 하연/mainserver2/test_main2.js
@@ -2,17 +2,81 @@ var gitTree = require('./git_log.js')();
 var flag = 0;
 
 
-function draw_handler(data) {
+function draw_handler(_finalObject) {
+	var log_diff_cnt = 0;
 	console.log("draw_handler======================");
-	console.log(typeof(data));
-	console.log(data);
+	console.log(typeof(_finalObject));	//object
+	console.log(_finalObject);
+
+	//var finalObject = {};
+	//finalObject.me = "";
+	//finalObject.array = final
 
 	//이 부분에서 ******************************
 	//사용자가 push나 pull을 누를때마다,
-	//받아온 finalArray를 가지고 ui 생성해주면 될듯
+	//받아온 _finalObject의 finalArray를 가지고 
+	//origin폴더와 _mahabono폴더의 commit hash를 비교해서
+	//ui 생성해주면 될듯
 	// ( origin 폴더의 git log가 갱신되는 시점은, 
 	//   사용자가 pull을 하고, 그다음 push를 하면 갱신된 log를 추가 )
 	//****************************************
+
+	//git log가 존재 안하면
+	if(_finalObject.origin_no_commits_flag || _finalObject.user__no_commits_flag) 
+	{
+		if(_finalObject.origin_no_commits_flag) 
+		{
+			console.log("origin no commit");
+			//***************************
+			//origin에 git log아무것도 없을 때의 동작
+			//***************************
+		}
+
+		if(_finalObject.user_no_commits_flag) 
+		{
+			console.log(_finalObject.my_name + " no commit");
+			//***************************
+			//_user_name folder에 git log아무것도 없을 때의 동작
+			//***************************
+		}
+		
+	}
+	//git log가 존재하면
+	else
+	{	
+		//log_diff_cnt 동작
+		for(idx in _finalObject.originLogTreeArray)
+		{
+			if(_finalObject.originLogTreeArray[idx].commit_hash !== _finalObject.my_position) 
+			{
+				log_diff_cnt++;
+			}	
+			else
+			{
+				break;
+			}
+		}
+		console.log("log_diff_cnt: " + log_diff_cnt);
+		
+		for(var i=0; i<log_diff_cnt; i++)
+		{
+			//*********************************
+			//update가 필요한 git log들을 그려준다.
+			//*********************************
+			console.log(_finalObject.originLogTreeArray[i].commit_hash);
+			console.log(_finalObject.originLogTreeArray[i].commit_name);
+			console.log(_finalObject.originLogTreeArray[i].commit_date);
+			console.log(_finalObject.originLogTreeArray[i].commit_date_rel);
+			console.log(_finalObject.originLogTreeArray[i].commit_msg);
+		}
+
+
+	}
+
 }
 
-gitTree.logTree('p_name', draw_handler);
+//gitTree.logTree('p_name','mahabono', draw_handler);	// draw_handler 테스트(V)
+//gitTree.logTree('p_name','jinoobono', draw_handler);	// draw_handler 테스트(V)
+
+gitTree.logTree('p_name','jinoobono', draw_handler);	// draw_handler 테스트2 - log있음(V) 
+gitTree.logTree('d','mahabono', draw_handler);	// draw_handler 테스트2 - log없음(V)
