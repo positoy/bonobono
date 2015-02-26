2d864e4df4c6441f968f795706b8da6b5f42ffa3
Choidora <choidora@localhost.localdomain>
2015-02-26 17:29:31 +0900

    폴더,파일추가/삭제 작동, 새로고침 버튼 작동; 파일 저장, 삭제, 생성 시 alert 말고 이쁜걸로 력 ;   기타 acd editor 버그 수정

---
+++ bono_v6.5/git.js
@@ -0,0 +1,422 @@
+var async = require('async');
+var fs = require('fs');
+var sys = require('sys');
+var exec = require('child_process').exec;
+
+var __DIR = './user_data/projects/';
+var __child = null;
+
+// child
+// login, view, create, join, invite
+// load, file reload, save, build, apk, edit, connection
+
+/////////////////////////////
+// CREATE BARE REPOSITORY
+/////////////////////////////
+exports.create = function(project_name, user_name, user_email, handler) {
+
+	var context = "[git create project] : ";
+	var DIR_PROJECT = __DIR + project_name;
+	var DIR_PROJECT_ORIGIN = DIR_PROJECT + "/origin";
+	var DIR_PROJECT_USER = DIR_PROJECT + "/_" + user_name;
+
+	console.log(context, project_name)
+
+	// condition check
+	var task0 = function(callback)
+	{
+		// directory duplicate must be checked.
+
+		callback(null);
+	};
+
+	// central repository
+	var task1 = function(callback)
+	{
+		var cmd = "git --bare init " + DIR_PROJECT_ORIGIN;
+		var child = exec(cmd, function(error, stdout, stderr) {
+
+			if (error !== null)
+			{
+				console.log(context, "error-", cmd);
+				console.log(error);
+			}
+			else
+			{
+				console.log(context, "successful-", cmd)
+				callback(null);
+			}
+		});
+	};
+
+	// clone repository
+	var task2 = function(callback)
+	{
+		var cmd = "git clone " + DIR_PROJECT_ORIGIN + " " + DIR_PROJECT_USER;
+		var child = exec(cmd, function(error, stdout, stderr) {
+
+			// directory duplicate must be checked.
+			if (error !== null)
+			{
+				console.log(context, "error-", cmd);
+				console.log(error);
+			}
+			else
+			{
+				console.log(context, "successful-", cmd);
+				callback(null);
+			}
+		});
+	};
+
+	// "git config --local user.name id"
+	// "git config --local user.email email"
+	// append string
+	// "[user]
+	//  	email = user_email
+	//		name = user_id
+	// "
+	// to '_user/.git/config' file.
+	var task3 = function(callback)
+	{
+		var str1 = "[user]\n";
+		var str2 = "\temail = " + user_email + "\n";
+		var str3 = "\tname = " + user_name + "\n";
+
+		var configFile = DIR_PROJECT_USER + "/.git/config";
+
+		fs.appendFile(configFile, str1+str2+str3, function(err) {
+			if (err)
+			{
+				throw err;
+			}
+			else
+			{
+				console.log("successful: config update.");
+				callback(null);
+			}
+		});
+	}
+
+	// clone repository를 안드로이드 프로젝트로 만들기
+	var task4 = function(callback)
+	{
+		var cmd = "android create project --name " + project_name + " --activity MainActivity --package com.bonobono." + project_name + " --target 10 --path " + DIR_PROJECT_USER;
+		var child = exec(cmd, function(error, stdout, stderr) {
+
+			if (error !== null)
+			{
+				console.log(context, "error-", cmd);
+				console.log(error);
+			}
+			else
+			{
+				console.log(context, "successful-", cmd)
+				callback(null);
+			}
+		});
+	};
+
+	// 빌드에 필요한 라이브러리 카피하기
+	var task5 = function(callback)
+	{
+		var cmd1 = "cp -rf ./user_data/build/appcompat_v7 " + DIR_PROJECT;
+		var cmd2 = "cp " + DIR_PROJECT + "/appcompat_v7/libs/android-support-v4.jar " + DIR_PROJECT_USER + "/libs"
+		//var cmd3 = "cp ./user_data/build/test.keystore " + DIR_PROJECT_USER;
+
+		var cmd = cmd1 + ";" + cmd2;
+
+		var child = exec(cmd, function(error, stdout, stderr) {
+
+			if (error !== null)
+			{
+				console.log(context, "error-", cmd);
+				console.log(error);
+			}
+			else
+			{
+				console.log(context, "successful-", cmd)
+				callback(null);
+			}
+		});
+	};
+
+	// git add, commit, push
+	var task6 = function(callback)
+	{
+		var cmd1 = "cd " + DIR_PROJECT_USER;
+		var cmd2 = "git add --a";
+		var cmd3 = 'git commit -m "first commit"'
+		var cmd4 = "git push origin master"
+
+		var cmd = cmd1 + ";" + cmd2 + ";" + cmd3 + ";" + cmd4;
+		var child = exec(cmd, function(error, stdout, stderr) {
+
+			if (error !== null)
+			{
+				console.log(context, "error-", cmd);
+				console.log(error);
+			}
+			else
+			{
+				console.log(context, "successful-", cmd)
+				callback(null, true);
+			}
+		});
+	};
+
+	// acknowledge for the "create request"
+	var callback = function(err, result)
+	{
+		if (result === true)
+			handler();
+	};
+
+	async.waterfall([task0, task1, task2, task3, task4, task5, task6], callback);
+};
+
+/****** execute a unix command with node.js MORE CONCISELY **
+
+var sys = require('sys')
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { console.log(stdout) }
+exec("ls -la", puts);
+
+*************************************************************/
+
+
+/////////////////////////////
+// JOIN A PROJECT
+/////////////////////////////
+exports.join = function(project_name, user_name, user_email, handler) {
+
+	var DIR_PROJECT = __DIR + project_name;
+	var DIR_PROJECT_ORIGIN = DIR_PROJECT + "/origin";
+	var DIR_PROJECT_USER = DIR_PROJECT + "/_" + user_name;
+
+	var goal = "\n[git] join(clone) '" + project_name + "' with id '" + user_name + "'";
+	console.log(goal);
+
+	// condition check
+	var task0 = function(callback)
+	{
+		// project existence must be checked.
+		// directory duplicate must be checked.
+		// project join permission must be checked.
+
+		callback(null);
+	};
+
+	// "git clone" origin project.
+	var task1 = function(callback)
+	{
+		var cmd = "git clone " + DIR_PROJECT_ORIGIN + " " + DIR_PROJECT_USER;
+		var child = exec(cmd, function(error, stdout, stderr) {
+
+			// directory duplicate must be checked.
+			if (error !== null)
+			{
+				console.log(error);
+			}
+			else
+			{
+				console.log("successful: " + cmd);
+				callback(null);
+			}
+		});
+	};
+
+	// "git config --local user.name id"
+	// "git config --local user.email email"
+	// append string
+	// "[user]
+	//  	email = user_email
+	//		name = user_id
+	// "
+	// to '_user/.git/config' file.
+	var task2 = function(callback)
+	{
+		var str1 = "[user]\n";
+		var str2 = "\temail = " + user_email + "\n";
+		var str3 = "\tname = " + user_name + "\n";
+
+		var configFile = DIR_PROJECT_USER + "/.git/config";
+
+		fs.appendFile(configFile, str1+str2+str3, function(err) {
+			if (err)
+			{
+				throw err;
+			}
+			else
+			{
+				console.log("successful: config update.");
+				callback(null, true);
+			}
+		});
+	}
+
+	// acknowledge for the "create request"
+	var callback = function(err, result)
+	{
+		if (result === true)
+			final_handler();
+	};
+
+	async.waterfall([task0, task1, task2], callback);
+};
+
+/////////////////////////////
+// PULL
+/////////////////////////////
+exports.pull = function(project_name, user_name, pull_handler, socket) {
+
+	console.log("project", project_name);
+	console.log("id", user_name);
+
+	var context = "[/editor, PULL] : ";
+
+	var DIR_PROJECT = __DIR + project_name;
+	var DIR_PROJECT_ORIGIN = DIR_PROJECT + "/origin";
+	var DIR_PROJECT_USER = DIR_PROJECT + "/_" + user_name;
+
+	var msg = "";
+
+	console.log(context, user_name, "request commit for project", project_name);
+
+	var task0 = function(callback)
+	{
+		//var cmd = "cd projects & cd " + project_name + "& cd _" + user_name + "&" + "git pull origin master";
+		var cmd = "cd " + DIR_PROJECT_USER + " ; " + "git pull --rebase origin master";
+		var child = exec(cmd, function(error, stdout, stderr) {
+
+			console.log(context, stdout);
+			msg += stdout;
+
+			if (error !== null)
+			{
+				console.log(context, "task0 error");
+				console.log(error);
+				pull_handler(false, socket, stderr);
+			}
+			else
+			{
+				callback(null, true);
+			}
+		});
+
+	};
+
+	// acknowledge for the "pull request"
+	var callback = function(err, pull_successful) {
+
+		if (pull_successful)
+		{
+			pull_handler(true, socket, msg);
+		}
+	};
+
+	async.waterfall([task0], callback);
+}
+
+
+/////////////////////////////
+// COMMIT
+/////////////////////////////
+exports.commit = function(project_name, user_name, commit_handler, socket) {
+
+	var context = "[/editor, COMMIT] : ";
+
+	var DIR_PROJECT = __DIR + project_name;
+	var DIR_PROJECT_ORIGIN = DIR_PROJECT + "/origin";
+	var DIR_PROJECT_USER = DIR_PROJECT + "/_" + user_name;
+
+	console.log(context, user_name, "request commit for project", project_name);
+
+	var task0 = function(callback)
+	{
+		//var cmd = "cd projects & cd " + project_name + "& cd _" + user_name + "&" + "git commit origin master";
+		var cmd1 = "cd " + DIR_PROJECT_USER;
+		var cmd2 = "git add --a";
+		var cmd3 = 'git commit -m "test message"';
+
+		var cmd = cmd1 + ';' + cmd2 + ';' + cmd3;
+
+		var child = exec(cmd, function(error, stdout, stderr) {
+
+			console.log(context, stdout);
+
+			console.log("error:", error);
+			if (error !== null)
+			{
+				console.log(context, "task0 error");
+				console.log(error);
+				commit_handler(false, socket, stderr);
+			}
+			else
+			{
+				callback(null, true);
+			}
+		});
+	};
+
+	// acknowledge for the "commit request"
+	var callback = function(err, commit_successful) {
+
+		if (commit_successful)
+		{
+			commit_handler(true, socket);
+		}
+	};
+
+	async.waterfall([task0], callback);
+}
+
+/////////////////////////////
+// PUSH
+/////////////////////////////
+exports.push = function(project_name, user_name, push_handler, socket) {
+
+	var context = "[/editor, PUSH] : ";
+
+	var DIR_PROJECT = __DIR + project_name;
+	var DIR_PROJECT_ORIGIN = DIR_PROJECT + "/origin";
+	var DIR_PROJECT_USER = DIR_PROJECT + "/_" + user_name;
+
+	console.log(context, user_name, "request push for project", project_name);
+
+	var task0 = function(callback)
+	{
+		var cmd1 = "cd " + DIR_PROJECT_USER;
+		var cmd2 = "git push origin master";
+
+		var cmd = cmd1 + ';' + cmd2;
+
+		var child = exec(cmd, function(error, stdout, stderr) {
+
+			console.log(context, stdout);
+
+			if (error !== null)
+			{
+				console.log(context, "task0 error");
+				console.log(error);
+				push_handler(false, socket, stderr);
+			}
+			else
+			{
+				callback(null, true);
+			}
+		});
+
+	};
+
+	// acknowledge for the "push request"
+	var callback = function(err, push_successful) {
+
+		if (push_successful)
+		{
+			push_handler(true, socket);
+		}
+	};
+
+	async.waterfall([task0], callback);
+}
